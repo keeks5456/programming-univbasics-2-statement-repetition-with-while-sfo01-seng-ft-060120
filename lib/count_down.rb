beb1363200a6c6af6c05b4519059ab0eaff5930d
Akiko Green <akikogreen08@gmail.com>
2020-04-21 04:56:44 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,12 +1,7 @@
 # Write your code here
 
-countdown = 1 
 count = 10 
-while count > 1 do
-  break if count == countdown do
-  puts "#{count}"
-  
-  count = count - 1
-  end 
+countdown = 1
+while count > 1 do 
+  puts
 end
-  puts "Happy New Year!"
